31a50d2076b8dbe905ccc4591c5b1bdc4e0fa603
MrFarhan <muhammadfarhan012345@@gmail.com>
2021-05-12 01:34:45 +0500

    used mongoose create method

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const TodoSchema = new mongoose.Schema({
     record: { type: String, required:true },
-    date: Number
+    date: {type:Number, default: Date.now}
 })
 
 const model = mongoose.model('TodoModel', TodoSchema)
--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const port = process.env.PORT || 3005
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Todo = require('./models/todo')
-console.log("todo required is " , Todo)
-console.log("todo from mongoose " , mongoose.model('TodoModel'))
+const cors = require('cors')
+
+mongoose.connect('mongodb://localhost/firstmongo')
 
 app.use("/", express.static(path.resolve(__dirname, 'Assets')))
-app.use(bodyParser.json())
+app.use(bodyParser())
 
+
+app.post('/api/create', async(req,res)=>{
+    const record = req.body
+    console.log(req.body)
+    const response = await Todo.create(record)
+    console.log("server response on create is " , response)
+    res.json({status:'ok'})
+})
 
 app.listen(port,() => {
     console.log('server is running on port : ' , port)
